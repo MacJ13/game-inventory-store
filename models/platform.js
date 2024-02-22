f703be4dce35733db7719eebb54cb96ec44e02c0
MacJ13 <jacdim02@gmail.com>
2024-02-22 23:56:02 +0100

    Create genre, platform and publisher model schemas

---
+++ models/platform.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PlatformSchema = new Schema({
+  name: { type: String, required: true },
+});
+
+PlatformSchema.virtual("url").get(function () {
+  return `/catalog/platform/${this._id}`;
+});
+
+module.exports = mongoose.model("Platform", PlatformSchema);
---
+++ models/publisher.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const PublisherSchema = new Schema({
+  name: { type: String, required: true },
+  country: { type: String, required: true },
+});
+
+PublisherSchema.virtual("url").get(function () {
+  return `/catalog/publisher/${this._id}`;
+});
+
+module.exports = mongoose.model("Publisher", PublisherSchema);
