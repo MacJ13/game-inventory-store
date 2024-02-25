22eb03c7d51c99d721efc45801c544a8f0e3aa20
MacJ13 <jacdim02@gmail.com>
2024-02-25 23:22:00 +0100

    Update index route with game controller

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const router = express.Router();
 
+const game_controller = require("../controllers/gameController");
+
 /* GET home page. */
-router.get("/", function (req, res, next) {
-  res.render("index", { title: "Express" });
-});
+// router.get("/", function (req, res, next) {
+//   res.render("index", { title: "Express" });
+// });
+
+router.get("/", game_controller.index);
 
 module.exports = router;
