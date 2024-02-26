ba2ee0a20879eb0ff6b28eadd924bba6bd812d63
MacJ13 <jacdim02@gmail.com>
2024-02-26 16:09:38 +0100

    Render view with publisher data list

--- controllers/publisherController.js
+++ controllers/publisherController.js
@@ -3,7 +3,12 @@ const asyncHandler = require("express-async-handler");
 
 // Display list of all Publishers
 exports.publisher_list = asyncHandler(async (req, res, next) => {
-  res.send("Not Implemented");
+  const publishers = await Publisher.find({}, "name").sort({ name: 1 }).exec();
+
+  res.render("publisher_list", {
+    title: "Publisher list",
+    publishers: publishers,
+  });
 });
 
 // Display publisher detual
