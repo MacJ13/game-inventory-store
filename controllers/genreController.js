a867138a66971554e07e4e96a515f799dba8a8e2
MacJ13 <jacdim02@gmail.com>
2024-02-26 20:41:54 +0100

    Render genre_list view with genre data model

--- controllers/genreController.js
+++ controllers/genreController.js
@@ -2,7 +2,9 @@ const Genre = require("../models/genre");
 const asyncHandler = require("express-async-handler");
 
 exports.genre_list = asyncHandler(async (req, res, next) => {
-  res.send("NOT IMPLEMENTED: Genre list");
+  const genres = await Genre.find({}).sort({ name: 1 }).exec();
+
+  res.render("genre_list", { title: "Genres", genres: genres });
 });
 
 exports.genre_detail = asyncHandler(async (req, res, next) => {
