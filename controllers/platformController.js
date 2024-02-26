4f33b6e433f2439e0f6cea487b65ea821ac5f5be
MacJ13 <jacdim02@gmail.com>
2024-02-26 21:18:16 +0100

    Render platform view with platform data

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -2,7 +2,9 @@ const Platform = require("../models/platform");
 const asyncHandler = require("express-async-handler");
 
 exports.platform_list = asyncHandler(async (req, res, next) => {
-  res.send("Not Implemented Platform list");
+  const platforms = await Platform.find({}).sort({ name: 1 }).exec();
+
+  res.render("platform_list", { title: "Platform List", platforms: platforms });
 });
 
 exports.platform_detail = asyncHandler(async (req, res, next) => {
