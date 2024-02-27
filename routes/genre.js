606528f01f94d738f01247de8616a58f523313ea
MacJ13 <jacdim02@gmail.com>
2024-02-27 10:50:08 +0100

    Add genre/create route

--- routes/genre.js
+++ routes/genre.js
@@ -4,6 +4,9 @@ const router = express.Router();
 const genre_controller = require("../controllers/genreController");
 
 router.get("/all", genre_controller.genre_list);
+
+router.get("/create", genre_controller.genre_create_get);
+
 router.get("/:id", genre_controller.genre_detail);
 
 module.exports = router;
