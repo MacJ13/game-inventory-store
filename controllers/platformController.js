8e895809315a1ffa393f2d1efd8035ba91826654
MacJ13 <jacdim02@gmail.com>
2024-02-27 12:05:02 +0100

    Handle platfrom create on POST

--- controllers/platformController.js
+++ controllers/platformController.js
@@ -1,6 +1,7 @@
 const Platform = require("../models/platform");
 const Game = require("../models/game");
 const asyncHandler = require("express-async-handler");
+const { body, validationResult } = require("express-validator");
 
 exports.platform_list = asyncHandler(async (req, res, next) => {
   const platforms = await Platform.find({}).sort({ name: 1 }).exec();
@@ -29,3 +30,42 @@ exports.platform_create_get = asyncHandler(async (req, res, next) => {
 });
 
 // handle platform create on POST
+exports.platform_create_post = [
+  body("name", "platform name should contain at least 2 characters")
+    .trim()
+    .toLowerCase()
+    .isLength({ min: 2 })
+    .escape(),
+  // Process request after validation and sanitization
+  asyncHandler(async (req, res, next) => {
+    // Extract the validation, errors from a request.
+    const errors = validationResult(req);
+
+    const platform = new Platform({ name: req.body.name });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render the form again with sanitized values/error messages.
+      res.render("platform_form", {
+        title: "Add Platform",
+        platform,
+        errors: errors.array(),
+      });
+      return;
+    } else {
+      // Data from form is valid
+      // Check if Platform with same name already exists.
+      const platfromExist = await Platform.findOne({
+        name: req.body.name,
+      }).exec();
+
+      if (platfromExist) {
+        // Platform exists, redirect to its detail page.
+        res.redirect(platfromExist.url);
+      } else {
+        // New platform save. Redirect to platform detail page.
+        await platform.save();
+        res.redirect(platform.url);
+      }
+    }
+  }),
+];
--- routes/platform.js
+++ routes/platform.js
@@ -4,7 +4,10 @@ const router = express.Router();
 const platform_controller = require("../controllers/platformController");
 
 router.get("/all", platform_controller.platform_list);
+
 router.get("/create", platform_controller.platform_create_get);
+
+router.post("/create", platform_controller.platform_create_post);
 
 router.get("/:id", platform_controller.platform_detail);
 
